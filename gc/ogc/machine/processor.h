20e1f4dfc50b0c4110df606619da3203eaab5127
Extrems <metaradil@gmail.com>
2021-06-20 16:31:15 -0400

    Fix constraints of ISR operations

--- gc/ogc/machine/processor.h
+++ gc/ogc/machine/processor.h
@@ -116,23 +116,19 @@
 			"mfmsr %0\n" \
 			"ori %0,%0,0x8000\n" \
 			"mtmsr %0" \
-			: "=&r" ((_val)) : "0" ((_val)) \
-			: : "memory" \
+			: "=&r" (_val) : : "memory" \
 		); \
 	} while (0)
 
 #define _CPU_ISR_Disable( _isr_cookie ) \
 	do { \
 		register u32 _disable_mask = 0; \
-		_isr_cookie = 0; \
 		__asm__ __volatile__ ( \
-			"mfmsr %0\n" \
-			"rlwinm %1,%0,0,17,15\n" \
-			"mtmsr %1\n" \
-			"extrwi %0,%0,1,16" \
-			: "=&r" ((_isr_cookie)), "=&r" ((_disable_mask)) \
-			: "0" ((_isr_cookie)), "1" ((_disable_mask)) \
-			: "memory" \
+			"mfmsr %1\n" \
+			"rlwinm %0,%1,0,17,15\n" \
+			"mtmsr %0\n" \
+			"extrwi %1,%1,1,16" \
+			: "=&r" (_disable_mask), "=&r" (_isr_cookie) : : "memory" \
 		); \
 	} while (0)
 
@@ -140,15 +136,13 @@
 	do { \
 		register u32 _enable_mask = 0; \
 		__asm__ __volatile__ ( \
-			"cmpwi %0,0\n" \
+			"cmpwi %1,0\n" \
 			"beq 1f\n" \
-			"mfmsr %1\n" \
-			"ori %1,%1,0x8000\n" \
-			"mtmsr %1\n" \
+			"mfmsr %0\n" \
+			"ori %0,%0,0x8000\n" \
+			"mtmsr %0\n" \
 			"1:" \
-			: "=r"((_isr_cookie)),"=&r" ((_enable_mask)) \
-			: "0"((_isr_cookie)),"1" ((_enable_mask)) \
-			: "memory" \
+			: "=&r" (_enable_mask) : "r" (_isr_cookie) : "memory" \
 		); \
 	} while (0)
 
@@ -156,17 +150,16 @@
 	do { \
 		register u32 _flash_mask = 0; \
 		__asm__ __volatile__ ( \
-			"cmpwi %0,0\n" \
+			"cmpwi %1,0\n" \
 			"beq 1f\n" \
-			"mfmsr %1\n" \
-			"ori %1,%1,0x8000\n" \
-			"mtmsr %1\n" \
-			"rlwinm %1,%1,0,17,15\n" \
-			"mtmsr %1\n" \
+			"mfmsr %0\n" \
+			"ori %0,%0,0x8000\n" \
+			"mtmsr %0\n" \
+			"mfmsr %0\n" \
+			"rlwinm %0,%0,0,17,15\n" \
+			"mtmsr %0\n" \
 			"1:" \
-			: "=r" ((_isr_cookie)), "=&r" ((_flash_mask)) \
-			: "0" ((_isr_cookie)), "1" ((_flash_mask)) \
-			: "memory" \
+			: "=&r" (_flash_mask) : "r" (_isr_cookie) : "memory" \
 		); \
 	} while (0)
 
