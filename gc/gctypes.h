3a02d4433480e641220ecf4082bc532f2a311fa4
Crayon <crayon1@rocketmail.com>
2023-12-22 12:44:09 -0500

    Fix typo (#162)
    thanks for the PR of the typo :)

--- gc/gctypes.h
+++ gc/gctypes.h
@@ -71,7 +71,7 @@ typedef unsigned int BOOL;
 /*+----------------------------------------------------------------------------------------------+*/
 #ifndef BIG_ENDIAN
 #define BIG_ENDIAN     1234
-#endif /* BIGE_ENDIAN */
+#endif /* BIG_ENDIAN */
 /*+----------------------------------------------------------------------------------------------+*/
 #ifndef BYTE_ORDER
 #define BYTE_ORDER     BIG_ENDIAN
