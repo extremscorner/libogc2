81587f3992f8a98d1ebf079159dd4de187dc9668
Extrems <metaradil@gmail.com>
2024-02-03 17:12:40 -0500

    Allocate kernel heap from MEM2 if there isn't enough space in MEM1

--- libogc/lwp_wkspace.c
+++ libogc/lwp_wkspace.c
@@ -31,8 +31,11 @@ void __lwp_wkspace_init(u32 size)
 {
 	void *heap_addr = NULL;
 
-	heap_addr = SYS_AllocArenaMemLo(size,PPC_CACHE_ALIGNMENT);
-
+	heap_addr = SYS_AllocArena1MemLo(size,PPC_CACHE_ALIGNMENT);
+#if defined(HW_RVL)
+	if(!heap_addr)
+		heap_addr = SYS_AllocArena2MemLo(size,PPC_CACHE_ALIGNMENT);
+#endif
 	memset(heap_addr,0,size);
 	__wkspace_heap_size = __lwp_heap_init(&__wkspace_heap,heap_addr,size,PPC_ALIGNMENT);
 }
