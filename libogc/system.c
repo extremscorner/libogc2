0fe614b2f94956c3019e96047ac1cad4dd329e39
Extrems <metaradil@gmail.com>
2024-07-03 14:05:08 -0400

    Handle unaligned DMA in __read_rom

--- libogc/system.c
+++ libogc/system.c
@@ -535,8 +535,6 @@ static u32 __read_rom(void *buf,u32 len,u32 offset)
 	u32 ret;
 	u32 loff;
 
-	DCInvalidateRange(buf,len);
-
 	if(EXI_Lock(EXI_CHANNEL_0,EXI_DEVICE_1,NULL)==0) return 0;
 	if(EXI_Select(EXI_CHANNEL_0,EXI_DEVICE_1,EXI_SPEED32MHZ)==0) {
 		EXI_Unlock(EXI_CHANNEL_0);
@@ -545,12 +543,11 @@ static u32 __read_rom(void *buf,u32 len,u32 offset)
 
 	ret = 0;
 	loff = offset<<6;
-	if(EXI_Imm(EXI_CHANNEL_0,&loff,4,EXI_WRITE,NULL)==0) ret |= 0x0001;
-	if(EXI_Sync(EXI_CHANNEL_0)==0) ret |= 0x0002;
-	if(EXI_Dma(EXI_CHANNEL_0,buf,len,EXI_READ,NULL)==0) ret |= 0x0004;
-	if(EXI_Sync(EXI_CHANNEL_0)==0) ret |= 0x0008;
-	if(EXI_Deselect(EXI_CHANNEL_0)==0) ret |= 0x0010;
-	if(EXI_Unlock(EXI_CHANNEL_0)==0) ret |= 0x00020;
+	if(EXI_Imm(EXI_CHANNEL_0,&loff,4,EXI_WRITE,NULL)==0) ret |= 0x01;
+	if(EXI_Sync(EXI_CHANNEL_0)==0) ret |= 0x02;
+	if(EXI_DmaEx(EXI_CHANNEL_0,buf,len,EXI_READ)==0) ret |= 0x04;
+	if(EXI_Deselect(EXI_CHANNEL_0)==0) ret |= 0x08;
+	if(EXI_Unlock(EXI_CHANNEL_0)==0) ret |= 0x10;
 
 	if(ret) return 0;
 	return 1;
