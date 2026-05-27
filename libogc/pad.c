d1d799d245f9a218aa1dfe68592ab04d4cd8ad64
Extrems <metaradil@gmail.com>
2026-05-27 17:42:14 -0400

    Use controller specification from loader

--- libogc/pad.c
+++ libogc/pad.c
@@ -35,10 +35,14 @@ typedef struct _keyinput {
 	u32 chan;
 } keyinput;
 
-typedef void (*SPECCallback)(u32,u32*,PADStatus*);
+typedef void (*SPECCallback)(u32 chan,u32 *data,PADStatus *status);
 
+static void SPEC0_MakeStatus(u32 chan,u32 *data,PADStatus *status);
+static void SPEC1_MakeStatus(u32 chan,u32 *data,PADStatus *status);
+static void SPEC2_MakeStatus(u32 chan,u32 *data,PADStatus *status);
+
+static SPECCallback __pad_makestatus = SPEC2_MakeStatus;
 static sampling_callback __pad_samplingcallback = NULL;
-static SPECCallback __pad_makestatus = NULL;
 static u32 __pad_initialized = 0;
 static u32 __pad_enabledbits = 0;
 static u32 __pad_resettingbits = 0;
@@ -524,12 +528,14 @@ u32 PAD_Init(void)
 {
 	u32 chan;
 	u16 prodpads = PAD_PRODPADS;
+	u8 *spec = (u8*)0x800030e9;
 #ifdef _PAD_DEBUG
 	printf("PAD_Init()\n");
 #endif
 	if(__pad_initialized) return 1;
 
-	if(__pad_spec) PAD_SetSpec(__pad_spec);
+	if(*spec) PAD_SetSpec(*spec);
+	__pad_initialized = 1;
 
 	memset(__pad_keys,0,sizeof(keyinput)*PAD_CHANMAX);
 
@@ -545,7 +551,6 @@ u32 PAD_Init(void)
 	SI_RefreshSamplingRate();
 	SYS_RegisterResetFunc(&pad_resetinfo);
 
-	__pad_initialized = 1;
 	return PAD_Reset(0xf0000000);
 }
 
@@ -751,10 +756,10 @@ void PAD_SetSpec(u32 spec)
 {
 	if(__pad_initialized) return;
 
-	__pad_spec = 0;
+	*(u8*)0x800030e9 = spec;
 	if(spec==0) __pad_makestatus = SPEC0_MakeStatus;
 	else if(spec==1) __pad_makestatus = SPEC1_MakeStatus;
-	else if(spec<6) __pad_makestatus = SPEC2_MakeStatus;
+	else if(spec<=5) __pad_makestatus = SPEC2_MakeStatus;
 
 	__pad_spec = spec;
 }
@@ -922,7 +927,6 @@ u32 PAD_ScanPads(void)
 				switch(n64status[i].err) {
 				case N64_ERR_READY:
 					state = 0;
-
 					if(n64status[i].button&N64_BUTTON_LEFT)		state |= PAD_BUTTON_LEFT;
 					if(n64status[i].button&N64_BUTTON_RIGHT)	state |= PAD_BUTTON_RIGHT;
 					if(n64status[i].button&N64_BUTTON_DOWN)		state |= PAD_BUTTON_DOWN;
